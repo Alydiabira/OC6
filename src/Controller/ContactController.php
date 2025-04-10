a114a2868601984e1edea2a78e183b761a648c9a
Aly <d-aly@outlook.fr>
2025-04-10 13:08:20 +0200

    Create ContactController.php

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use App\Entity\Contact;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Forward l'entité Contact à MailController
+            return $this->forward('App\Controller\MailController::send', [
+                'contact' => $contact,  // Passe l'objet Contact ici
+            ]);
+        }
+
+        return $this->render('contact/contact.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
