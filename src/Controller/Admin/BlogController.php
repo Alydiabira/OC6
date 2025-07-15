217f0ce1f10a15adf9dafd90f75769a4a4dcce18
Aly <d-aly@outlook.fr>
2025-07-15 15:21:37 +0200

    Update BlogController.php

--- src/Controller/Admin/BlogController.php
+++ src/Controller/Admin/BlogController.php
@@ -6,7 +6,6 @@ use App\Entity\Post;
 use App\Entity\User;
 use App\Entity\Comment;
 use App\Form\PostType;
-use App\Form\UserType;
 use App\Repository\PostRepository;
 use App\Security\PostVoter;
 use Doctrine\ORM\EntityManagerInterface;
@@ -19,15 +18,20 @@ use Symfony\Component\Routing\Requirement\Requirement;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use App\Form\UserType;
 
 /**
  * Controller used to manage blog contents in the backend.
  */
-#[Route('/{_locale}/admin/post', name: 'admin_', defaults: ['_locale' => 'fr'])]
+#[Route('/admin/post')]
 #[IsGranted(User::ROLE_ADMIN)]
-final class AdminController extends AbstractController
+final class BlogController extends AbstractController
 {
-    #[Route('/', name: 'post_index', methods: ['GET'])]
+    /**
+     * Lists all Post entities.
+     */
+    #[Route('/', name: 'admin_index', methods: ['GET'])]
+    #[Route('/', name: 'admin_post_index', methods: ['GET'])]
     public function index(
         #[CurrentUser] User $user,
         PostRepository $posts,
@@ -37,7 +41,10 @@ final class AdminController extends AbstractController
         return $this->render('admin/blog/index.html.twig', ['posts' => $authorPosts]);
     }
 
-    #[Route('/new', name: 'post_new', methods: ['GET', 'POST'])]
+    /**
+     * Creates a new Post entity.
+     */
+    #[Route('/new', name: 'admin_post_new', methods: ['GET', 'POST'])]
     public function new(
         #[CurrentUser] User $user,
         Request $request,
@@ -45,75 +52,86 @@ final class AdminController extends AbstractController
     ): Response {
         $post = new Post();
         $post->setAuthor($user);
-
+    
         $form = $this->createForm(PostType::class, $post)
             ->add('saveAndCreateNew', SubmitType::class);
-
+    
         $form->handleRequest($request);
-
+    
         if ($form->isSubmitted() && $form->isValid()) {
+            // Gestion de l'image
             $imageFile = $form->get('imageFile')->getData();
             if ($imageFile) {
                 $post->setImageFile($imageFile);
             }
-
+    
             $entityManager->persist($post);
             $entityManager->flush();
-
+    
             $this->addFlash('success', 'post.created_successfully');
-
+    
             return $this->redirectToRoute('admin_post_index', [], Response::HTTP_SEE_OTHER);
         }
-
+    
         return $this->render('admin/blog/new.html.twig', [
             'post' => $post,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'post_show', requirements: ['id' => Requirement::POSITIVE_INT], methods: ['GET'])]
+    /**
+     * Finds and displays a Post entity.
+     */
+    #[Route('/{id}', name: 'admin_post_show', requirements: ['id' => Requirement::POSITIVE_INT], methods: ['GET'])]
     public function show(Post $post): Response
     {
-        $this->denyAccessUnlessGranted(PostVoter::SHOW, $post);
+        $this->denyAccessUnlessGranted(PostVoter::SHOW, $post, 'Posts can only be shown to their authors.');
 
         return $this->render('admin/blog/show.html.twig', [
             'post' => $post,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'post_edit', requirements: ['id' => Requirement::POSITIVE_INT], methods: ['GET', 'POST'])]
-    #[IsGranted('edit', subject: 'post')]
+    /**
+     * Displays a form to edit an existing Post entity.
+     */
+    #[Route('/{id}/edit', name: 'admin_post_edit', requirements: ['id' => Requirement::POSITIVE_INT], methods: ['GET', 'POST'])]
+    #[IsGranted('edit', subject: 'post', message: 'Posts can only be edited by their authors.')]
     public function edit(Request $request, Post $post, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
-
+    
         if ($form->isSubmitted() && $form->isValid()) {
+            // Gestion de l'image
             $imageFile = $form->get('imageFile')->getData();
             if ($imageFile) {
                 $post->setImageFile($imageFile);
             }
-
+    
             $entityManager->flush();
             $this->addFlash('success', 'post.updated_successfully');
-
+    
             return $this->redirectToRoute('admin_post_edit', ['id' => $post->getId()], Response::HTTP_SEE_OTHER);
         }
-
+    
         return $this->render('admin/blog/edit.html.twig', [
             'post' => $post,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}/delete', name: 'post_delete', requirements: ['id' => Requirement::POSITIVE_INT], methods: ['POST'])]
+    /**
+     * Deletes a Post entity.
+     */
+    #[Route('/{id}/delete', name: 'admin_post_delete', requirements: ['id' => Requirement::POSITIVE_INT], methods: ['POST'])]
     #[IsGranted('delete', subject: 'post')]
     public function delete(Request $request, Post $post, EntityManagerInterface $entityManager): Response
     {
         $token = $request->get('token');
-
+        
         if (!$this->isCsrfTokenValid('delete', $token)) {
-            return $this->redirectToRoute('admin_post_index');
+            return $this->redirectToRoute('admin_post_index', [], Response::HTTP_SEE_OTHER);
         }
 
         $post->getTags()->clear();
@@ -122,47 +140,64 @@ final class AdminController extends AbstractController
 
         $this->addFlash('success', 'post.deleted_successfully');
 
+        return $this->redirectToRoute('admin_post_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
+/**
+ * Deletes a Comment entity (Only for admin users).
+ */
+#[Route('/comment/{commentId}/delete', name: 'admin_comment_delete', requirements: ['commentId' => Requirement::POSITIVE_INT], methods: ['POST'])]
+#[IsGranted('ROLE_ADMIN')]  // Ensure only admin can delete comments
+public function commentDelete(
+    #[MapEntity(mapping: ['commentId' => 'id'])] Comment $comment,  // Automatically fetch the Comment entity using the commentId
+    EntityManagerInterface $entityManager,
+    Request $request
+): Response {
+    // CSRF protection
+    $token = $request->get('token');
+    if (!$this->isCsrfTokenValid('delete_comment', $token)) {
         return $this->redirectToRoute('admin_post_index');
     }
 
-    #[Route('/comment/{commentId}/delete', name: 'comment_delete', requirements: ['commentId' => Requirement::POSITIVE_INT], methods: ['POST'])]
-    public function commentDelete(
-        #[MapEntity(mapping: ['commentId' => 'id'])] Comment $comment,
-        EntityManagerInterface $entityManager,
-        Request $request
-    ): Response {
-        $token = $request->get('token');
-        if (!$this->isCsrfTokenValid('delete_comment', $token)) {
-            return $this->redirectToRoute('admin_post_index');
-        }
-
+    // Vérification si le commentaire existe
+    if ($comment) {
+        // Suppression du commentaire
         $entityManager->remove($comment);
         $entityManager->flush();
 
+        // Message de succès
         $this->addFlash('success', 'comment.deleted_successfully');
-
-        return $this->redirectToRoute('admin_post_show', ['id' => $comment->getPost()->getId()]);
+    } else {
+        // Message d'erreur si le commentaire n'est pas trouvé
+        $this->addFlash('error', 'comment.not_found');
     }
 
-    #[Route('/profile', name: 'profile', methods: ['GET', 'POST'])]
-    public function profile(
-        #[CurrentUser] User $user,
-        Request $request,
-        EntityManagerInterface $entityManager
-    ): Response {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+    // Redirection vers la page de l'article
+    return $this->redirectToRoute('admin_post_show', ['id' => $comment->getPost()->getId()]);
+}
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-            $this->addFlash('success', 'Profil mis à jour avec succès.');
+#[Route('/profile', name: 'admin_profile', methods: ['GET', 'POST'])]
+#[IsGranted('ROLE_ADMIN')]
+public function profile(
+    #[CurrentUser] User $user,
+    Request $request,
+    EntityManagerInterface $entityManager
+): Response {
+    $form = $this->createForm(UserType::class, $user);
+    $form->handleRequest($request);
 
-            return $this->redirectToRoute('admin_profile');
-        }
-
-        return $this->render('admin/profile/edit.html.twig', [
-            'form' => $form->createView(),
-            'user' => $user,
-        ]);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $entityManager->flush();
+        $this->addFlash('success', 'Profil mis à jour avec succès.');
+        return $this->redirectToRoute('admin_profile');
     }
+
+    return $this->render('admin/profile/edit.html.twig', [
+        'form' => $form->createView(),
+        'user' => $user,
+    ]);
+}
+
+
 }
