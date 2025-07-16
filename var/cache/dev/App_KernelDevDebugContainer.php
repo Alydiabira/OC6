d87dee801f52ebd5f30f9456b2cf3eac41f8ee0e
Aly <d-aly@outlook.fr>
2025-07-16 13:35:41 +0200

    Update App_KernelDevDebugContainer.php

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerXujzwlG\App_KernelDevDebugContainer([
     'container.build_hash' => 'XujzwlG',
-    'container.build_id' => '810eb75a',
-    'container.build_time' => 1752663387,
+    'container.build_id' => 'cf5a6bc5',
+    'container.build_time' => 1752665717,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerXujzwlG');
