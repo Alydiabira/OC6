136804d0b8e2ad9cd70f60268d6fe399d89cfa7c
Aly <d-aly@outlook.fr>
2025-07-23 13:58:24 +0200

    Update Book.php

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -50,6 +50,22 @@ class Book
 
     #[ORM\ManyToMany(targetEntity: Tag::class, inversedBy: 'books')]
     private Collection $tags;
+    
+    #[ORM\Column(length: 255, unique: true)]
+    #[Assert\NotBlank]
+    private ?string $slug = null;
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+        return $this;
+    }
+
 
 
     public function __construct()
