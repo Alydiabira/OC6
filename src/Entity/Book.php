43fef789e3b0bee2444ad91d01d3f83c4c6a2dd3
Aly <d-aly@outlook.fr>
2025-07-23 13:46:40 +0200

    Update Book.php

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -6,6 +6,10 @@ use App\Repository\BookRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use App\Entity\User;
+use App\Entity\Tag;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
 class Book
@@ -41,12 +45,17 @@ class Book
     private ?User $owner = null;
 
     #[ORM\ManyToOne(targetEntity: User::class)]
-    #[ORM\JoinColumn(nullable: false)]
+    #[ORM\JoinColumn(nullable: true)]
     private ?User $seller = null;
+
+    #[ORM\ManyToMany(targetEntity: Tag::class, inversedBy: 'books')]
+    private Collection $tags;
+
 
     public function __construct()
     {
         $this->createdAt = new \DateTimeImmutable();
+        $this->tags = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -139,6 +148,30 @@ class Book
     public function setSeller(?User $seller): static
     {
         $this->seller = $seller;
+        return $this;
+    }
+
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
+
+    public function addTag(Tag $tag): static
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags->add($tag);
+            $tag->addBook($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): static
+    {
+        if ($this->tags->removeElement($tag)) {
+            $tag->removeBook($this);
+        }
+
         return $this;
     }
 }
