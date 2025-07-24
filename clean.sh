637ee1788e8f12135989e620519ce5d42814d269
Aly <d-aly@outlook.fr>
2025-07-24 15:56:16 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
 
-x
+echo "🧹 Suppression du cache Symfony..."
+rm -rf var/cache/*
+
+echo "🧹 Suppression du cache Twig compilé..."
+rm -rf var/cache/dev/twig
+
+echo "🔥 Reconstruction du cache..."
+php bin/console cache:clear
+php bin/console cache:warmup
+
+echo "✅ Symfony est propre. Tu peux relancer le serveur."
