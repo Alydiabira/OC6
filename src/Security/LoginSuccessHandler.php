34e30796753428aa3b9d1bd8a01764e5d677a1b2
Aly <d-aly@outlook.fr>
2025-07-29 11:11:18 +0200

    Create LoginSuccessHandler.php

---
+++ src/Security/LoginSuccessHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    public function __construct(private UrlGeneratorInterface $urlGenerator) {}
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
+    {
+        $user = $token->getUser();
+
+        if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+            return new RedirectResponse($this->urlGenerator->generate('admin_post_index'));
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate('blog_index'));
+    }
+}
