824bc43ffc3e20f53d2680daab35b24fb1dffd88
Aly <d-aly@outlook.fr>
2025-07-29 12:47:15 +0200

    Update LoginSuccessHandler.php

--- src/Security/LoginSuccessHandler.php
+++ src/Security/LoginSuccessHandler.php
@@ -4,19 +4,25 @@ namespace App\Security;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
 {
-    public function __construct(private UrlGeneratorInterface $urlGenerator) {}
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
     {
         $user = $token->getUser();
 
-        if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+        // Vérifie que l'utilisateur est bien un objet User
+        if (method_exists($user, 'getRoles') && in_array('ROLE_ADMIN', $user->getRoles(), true)) {
             return new RedirectResponse($this->urlGenerator->generate('admin_post_index'));
         }
 
