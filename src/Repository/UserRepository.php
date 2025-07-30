6dcef7f023ba3e1595519b3b17f3f0a06b9eb6a9
Aly <d-aly@outlook.fr>
2025-07-30 13:23:37 +0200

    Update UserRepository.php

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -16,14 +16,6 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * This custom Doctrine repository is empty because so far we don't need any custom
- * method to query for application user information. But it's always a good practice
- * to define a custom repository that will be used when the application grows.
- *
- * See https://symfony.com/doc/current/doctrine.html#querying-for-objects-the-repository
- *
- * @author Ryan Weaver <weaverryan@gmail.com>
- * @author Javier Eguiluz <javier.eguiluz@gmail.com>
  *
  * @method User|null findOneByUsername(string $username)
  * @method User|null findOneByEmail(string $email)
