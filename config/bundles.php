b7d58bf4275a2235b7fd4ba2f9c9e06994007a83
Aly <d-aly@outlook.fr>
2025-07-30 11:25:20 +0200

    Update bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\UX\Translator\UxTranslatorBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
